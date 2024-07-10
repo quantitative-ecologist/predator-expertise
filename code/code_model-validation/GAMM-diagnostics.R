206020332fc6e7fd83e12bb7a105b4415739401f
Maxime Fraser Franco <49170767+quantitative-ecologist@users.noreply.github.com>
2024-07-10 07:37:28 -0400

    update diagnostics

--- code/code_model-validation/GAMM-diagnostics.R
+++ code/code_model-validation/GAMM-diagnostics.R
@@ -16,27 +16,26 @@
 
 # Librairies ------------------------------------------------------------
 
- # Detect cores
- options(mc.cores = parallel::detectCores())
+# Detect cores
+options(mc.cores = parallel::detectCores())
 
- library(data.table)
- library(brms)
- library(parallel)
- library(ggpubr)
+library(data.table)
+library(brms)
+library(parallel)
+library(ggpubr)
 
 
 
 # Import the model ------------------------------------------------------
 
- path <- file.path(getwd(), "outputs", "01_outputs_models")
- #path <- getwd()
+path <- file.path(getwd(), "outputs", "outputs_models")
 
- # Load models
- mod1 <- readRDS(file.path(path, "A1_GAMM-rank.rds"))
- mod2 <- readRDS(file.path(path, "A2_GAMM-rank.rds"))
- mod3 <- readRDS(file.path(path, "A3_GAMM-rank.rds"))
- mod4 <- readRDS(file.path(path, "A2_GAMM-speed-rank.rds"))
- mod5 <- readRDS(file.path(path, "A3_GAMM-speed-rank.rds"))
+# Load models
+mod1 <- readRDS(file.path(path, "A1_GAMM-rank.rds"))
+mod2 <- readRDS(file.path(path, "A2_GAMM-rank.rds"))
+mod3 <- readRDS(file.path(path, "A3_GAMM-rank.rds"))
+mod4 <- readRDS(file.path(path, "A2_GAMM-speed-rank.rds"))
+mod5 <- readRDS(file.path(path, "A3_GAMM-speed-rank.rds"))
 
  # Check object size
  #print(object.size(mod1), units = "MB")
@@ -100,122 +99,139 @@
 
 # Trace plots and parameter distributions -------------------------------
 
- #plot(mod1)
- #plot(mod2)
- #plot(mod3)
- #plot(mod4)
- #plot(mod5)
+plot(mod1)
+plot(mod2)
+plot(mod3)
+plot(mod4)
+plot(mod5)
 
 
 
 # Posterior predictive checks -------------------------------------------
 
- # Check distributions
- pp1 <- pp_check(mod1)
- pp1 <- pp1 + ggtitle("GAMM - A1") +
-  theme(legend.position = "bottom")
- pp2 <- pp_check(mod2)
- pp2 <- pp2 + ggtitle("GAMM - A2") +
-  theme(legend.position = "bottom")
- pp3 <- pp_check(mod3)
- pp3 <- pp3 + ggtitle("GAMM - A3") +
-  theme(legend.position = "bottom")
- pp4 <- pp_check(mod4)
- pp4 <- pp4 + ggtitle("GAMM - A2 prey") +
-  theme(legend.position = "bottom")
- pp5 <- pp_check(mod5)
- pp5 <- pp5 + ggtitle("GAMM - A3 prey") +
-  theme(legend.position = "bottom")
+# Check distributions
+pp1 <- pp_check(mod1)
+pp1 <- pp1 + ggtitle("GAMM - A1 rank") +
+ theme(legend.position = "bottom")
 
- # Effects plot (mean variance plot)
- stat1 <- pp_check(mod1, type = "stat_2d")
- stat1 <- stat1 + ggtitle("GAMM - A1") +
-  theme(legend.position = "bottom")
- stat2 <- pp_check(mod2, type = "stat_2d")
- stat2 <- stat2 + ggtitle("GAMM - A2") +
-  theme(legend.position = "bottom")
- stat3 <- pp_check(mod3, type = "stat_2d")
- stat3 <- stat3 + ggtitle("GAMM - A3") +
-  theme(legend.position = "bottom")
- stat4 <- pp_check(mod4, type = "stat_2d")
- stat4 <- stat4 + ggtitle("GAMM - A2 prey") +
-  theme(legend.position = "bottom")
- stat5 <- pp_check(mod5, type = "stat_2d")
- stat5 <- stat5 + ggtitle("GAMM - A3 prey") +
-  theme(legend.position = "bottom")
+pp2 <- pp_check(mod2)
+pp2 <- pp2 + ggtitle("GAMM - A2 rank") +
+ theme(legend.position = "bottom")
 
- # Predicted means
- mean1 <- pp_check(mod1, type = "stat", stat = "mean")
- mean1 <- mean1 + ggtitle("GAMM - A1") +
-  theme(legend.position = "bottom")
- mean2 <- pp_check(mod2, type = "stat", stat = "mean")
- mean2 <- mean2 + ggtitle("GAMM - A2") +
-  theme(legend.position = "bottom")
- mean3 <- pp_check(mod3, type = "stat", stat = "mean")
- mean3 <- mean3 + ggtitle("GAMM - A3") +
-  theme(legend.position = "bottom")
- mean4 <- pp_check(mod4, type = "stat", stat = "mean")
- mean4 <- mean4 + ggtitle("GAMM - A2 prey") +
-  theme(legend.position = "bottom")
- mean5 <- pp_check(mod5, type = "stat", stat = "mean")
- mean5 <- mean5 + ggtitle("GAMM - A3 prey") +
-  theme(legend.position = "bottom")
+pp3 <- pp_check(mod3)
+pp3 <- pp3 + ggtitle("GAMM - A3 rank") +
+ theme(legend.position = "bottom")
 
- # Error scatter
- e_scat1 <- pp_check(mod1, type = "error_scatter_avg")
- e_scat1 <- e_scat1 + ggtitle("GAMM - A1") +
-  theme(legend.position = "bottom")
- e_scat2 <- pp_check(mod2, type = "error_scatter_avg")
- e_scat2 <- e_scat2 + ggtitle("GAMM - A2") +
-  theme(legend.position = "bottom")
- e_scat3 <- pp_check(mod3, type = "error_scatter_avg")
- e_scat3 <- e_scat3 + ggtitle("GAMM - A3") +
-  theme(legend.position = "bottom")
- e_scat4 <- pp_check(mod4, type = "error_scatter_avg")
- e_scat4 <- e_scat4 + ggtitle("GAMM - A2 prey") +
-  theme(legend.position = "bottom")
- e_scat5 <- pp_check(mod5, type = "error_scatter_avg")
- e_scat5 <- e_scat5 + ggtitle("GAMM - A3 prey") +
-  theme(legend.position = "bottom")
+pp4 <- pp_check(mod4)
+pp4 <- pp4 + ggtitle("GAMM - A2 rank + speed") +
+ theme(legend.position = "bottom")
+
+pp5 <- pp_check(mod5)
+pp5 <- pp5 + ggtitle("GAMM - A3 rank + speed") +
+ theme(legend.position = "bottom")
+
+
+# Effects plot (mean variance plot)
+stat1 <- pp_check(mod1, type = "stat_2d")
+stat1 <- stat1 + ggtitle("GAMM - A1 rank") +
+ theme(legend.position = "bottom")
+
+stat2 <- pp_check(mod2, type = "stat_2d")
+stat2 <- stat2 + ggtitle("GAMM - A2 rank") +
+ theme(legend.position = "bottom")
+
+stat3 <- pp_check(mod3, type = "stat_2d")
+stat3 <- stat3 + ggtitle("GAMM - A3 rank") +
+ theme(legend.position = "bottom")
+
+stat4 <- pp_check(mod4, type = "stat_2d")
+stat4 <- stat4 + ggtitle("GAMM - A2 rank + speed") +
+ theme(legend.position = "bottom")
+
+stat5 <- pp_check(mod5, type = "stat_2d")
+stat5 <- stat5 + ggtitle("GAMM - A3 rank + speed") +
+ theme(legend.position = "bottom")
+
+
+# Predicted means
+mean1 <- pp_check(mod1, type = "stat", stat = "mean")
+mean1 <- mean1 + ggtitle("GAMM - A1") +
+ theme(legend.position = "bottom")
+
+mean2 <- pp_check(mod2, type = "stat", stat = "mean")
+mean2 <- mean2 + ggtitle("GAMM - A2") +
+ theme(legend.position = "bottom")
+
+mean3 <- pp_check(mod3, type = "stat", stat = "mean")
+mean3 <- mean3 + ggtitle("GAMM - A3") +
+ theme(legend.position = "bottom")
+
+mean4 <- pp_check(mod4, type = "stat", stat = "mean")
+mean4 <- mean4 + ggtitle("GAMM - A2 prey") +
+ theme(legend.position = "bottom")
+
+mean5 <- pp_check(mod5, type = "stat", stat = "mean")
+mean5 <- mean5 + ggtitle("GAMM - A3 prey") +
+ theme(legend.position = "bottom")
+
+
+# Error scatter
+e_scat1 <- pp_check(mod1, type = "error_scatter_avg")
+e_scat1 <- e_scat1 + ggtitle("GAMM - A1 rank") +
+ theme(legend.position = "bottom")
+
+e_scat2 <- pp_check(mod2, type = "error_scatter_avg")
+e_scat2 <- e_scat2 + ggtitle("GAMM - A2 rank") +
+ theme(legend.position = "bottom")
+
+e_scat3 <- pp_check(mod3, type = "error_scatter_avg")
+e_scat3 <- e_scat3 + ggtitle("GAMM - A3 rank") +
+ theme(legend.position = "bottom")
+
+e_scat4 <- pp_check(mod4, type = "error_scatter_avg")
+e_scat4 <- e_scat4 + ggtitle("GAMM - A2 rank + speed") +
+ theme(legend.position = "bottom")
+
+e_scat5 <- pp_check(mod5, type = "error_scatter_avg")
+e_scat5 <- e_scat5 + ggtitle("GAMM - A3 rank + speed") +
+ theme(legend.position = "bottom")
 
 
 
 # Save plots as figure --------------------------------------------------
 
- plots <- list(
-  # Arrange a figure
-  stat_fig1 <- ggarrange(
-   pp1, stat1, mean1, e_scat1,
-   ncol = 2, nrow = 2
-  ),
+plots <- list(
+ # Arrange a figure
+ stat_fig1 <- ggarrange(
+  pp1, stat1, mean1, e_scat1,
+  ncol = 2, nrow = 2
+ ),
 
-  stat_fig2 <- ggarrange(
-   pp2, stat2, mean2, e_scat2,
-   ncol = 2, nrow = 2
-  ),
+ stat_fig2 <- ggarrange(
+  pp2, stat2, mean2, e_scat2,
+  ncol = 2, nrow = 2
+ ),
 
-  stat_fig3 <- ggarrange(
-   pp3, stat3, mean3, e_scat3,
-   ncol = 2, nrow = 2
-  ),
+ stat_fig3 <- ggarrange(
+  pp3, stat3, mean3, e_scat3,
+  ncol = 2, nrow = 2
+ ),
 
-  stat_fig4 <- ggarrange(
-   pp4, stat4, mean4, e_scat4,
-   ncol = 2, nrow = 2
-  ),
+ stat_fig4 <- ggarrange(
+  pp4, stat4, mean4, e_scat4,
+  ncol = 2, nrow = 2
+ ),
 
-  stat_fig5 <- ggarrange(
-   pp5, stat5, mean5, e_scat5,
-   ncol = 2, nrow = 2
-  )
+ stat_fig5 <- ggarrange(
+  pp5, stat5, mean5, e_scat5,
+  ncol = 2, nrow = 2
  )
+)
 
-  # Export the figures into a .pdf file
-  ggexport(
-    plots,
-    filename = file.path(path, "GAMM-diagnostics.pdf")
-  )
-
-
+# Export the figures into a .pdf file
+#ggexport(
+#  plots,
+#  filename = file.path(path, "GAMM-diagnostics.pdf")
+#)
 # =======================================================================
 # =======================================================================
