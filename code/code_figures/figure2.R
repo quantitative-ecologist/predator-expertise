bbc558f73601c19d8f38f9b316d3ff5015bb907f
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2025-05-19 12:57:05 -0400

    linux --> update fig2

--- code/code_figures/figure2.R
+++ code/code_figures/figure2.R
@@ -16,19 +16,18 @@
 # ==========================================================================
 
 
-
 # Load libraries and model -------------------------------------------------
 
 library(brms)
 library(data.table)
 library(ggplot2)
+library(ggridges)
 
 path <- file.path(getwd(), "outputs", "outputs_models")
 
-path <- file.path(getwd(), "outputs", "outputs_models2024")
-
-fit1 <- readRDS(file.path(path, "GAMM-II.rds"))
-fit2 <- readRDS(file.path(path, "GAMM-V.rds"))
+fit1 <- readRDS(file.path(path, "GAMM-I.rds"))
+fit2 <- readRDS(file.path(path, "GAMM-II.rds"))
+fit5 <- readRDS(file.path(path, "GAMM-V.rds"))
 
 # ==========================================================================
 # ==========================================================================
@@ -41,7 +40,7 @@ fit2 <- readRDS(file.path(path, "GAMM-V.rds"))
 # 2. Extract posterior draws into a tidy table
 # ==========================================================================
 
-# Extract posterior draws fit1
+# Extract posterior draws fit
 post1 <- data.table(
   as_draws_df(
     fit1,
@@ -53,7 +52,8 @@ post1 <- data.table(
   )
 )
 
-# Extract posterior draws fit2
+
+# Extract posterior draws fit1
 post2 <- data.table(
   as_draws_df(
     fit2,
@@ -65,18 +65,32 @@ post2 <- data.table(
   )
 )
 
+# Extract posterior draws fit2
+post5 <- data.table(
+  as_draws_df(
+    fit5,
+    variable = c(
+      "sds_sZcumul_xppredator_id_1",
+      "sds_sZcumul_xppredator_id_2",
+      "sds_sZcumul_xppredator_id_3"
+    )
+  )
+)
+
 # Clear memory
 rm(fit1)
 rm(fit2)
+rm(fit5)
 
 # Combine posterior draws
 figdat <- rbind(
   post1,
-  post2
+  post2,
+  post5
 )
 
 # Add model variable
-figdat[, model := c(rep("fit1", 1000), rep("fit2", 1000))]
+figdat[, model := c(rep("fit1", 4000), rep("fit2", 4000), rep("fit5", 4000))]
 
 # ==========================================================================
 # ==========================================================================
@@ -110,18 +124,6 @@ compute_results <- function(dt, col_name) {
   )
 }
 
-# Compute the results function for each model and each column
-# Assign to results data.table
-results <- figdat[
-  , {
-    rbind(
-      compute_results(.SD, "sds_sZcumul_xppredator_id_1"),
-      compute_results(.SD, "sds_sZcumul_xppredator_id_2"),
-      compute_results(.SD, "sds_sZcumul_xppredator_id_3")
-    )
-  }
-  , by = model
-]
 # ==========================================================================
 # ==========================================================================
 
@@ -142,29 +144,15 @@ figdat_long <- melt(
   value.name = "value"
 )
 
-# Relabel
-results[
-  , column := factor(
-    column,
-    levels = c("sds_sZcumul_xppredator_id_1",
-               "sds_sZcumul_xppredator_id_2",
-               "sds_sZcumul_xppredator_id_3"),
-    labels = c("Individual intercepts",
-               "Individual slopes",
-               "Individual wiggliness")
-  )
-]
-setnames(results, "column", "parameter")
-
 figdat_long[
   , parameter := factor(
     parameter,
     levels = c("sds_sZcumul_xppredator_id_1",
                "sds_sZcumul_xppredator_id_2",
                "sds_sZcumul_xppredator_id_3"),
-    labels = c("Individual intercepts",
-               "Individual slopes",
-               "Individual wiggliness")
+    labels = c("Individual\nintercepts",
+               "Individual\nslopes",
+               "Individual\nwiggliness")
   )
 ]
 # ==========================================================================
@@ -191,92 +179,25 @@ custom_theme <- theme(
   panel.background = element_blank(),
   legend.title = element_text(size = 13),
   legend.text = element_text(size = 13),
+  #legend.position = c(.73, .9),
   legend.position = "top"
 )
 
-# Basic plot variables
 p <- ggplot(
   data = figdat_long,
-  aes(x = value, fill = model, color = model)
+  aes(x = value, y = parameter, fill = model)
 )
 
-# Customize
-p <- p + geom_density(
-  alpha = 0.4,
-  position = "identity",
-  trim = TRUE
-) +
-  geom_vline(
-    data = results,
-    aes(xintercept = median, color = model),
-    linetype = "dashed",
-    show.legend = FALSE
-  ) +
-  scale_color_manual(
-    values = c("#482173FF", "#51C56AFF"),
-    labels = c("Prey rank", "Prey rank + prey speed")
-  ) +
-  scale_fill_manual(
-    values = c("#482173FF", "#51C56AFF"),
-    labels = c("Prey rank", "Prey rank + prey speed")
-  ) +
-  facet_wrap(~parameter, scales = "fixed") +
-  labs(fill = "Model:", color = "Model:") +
-  ylab("Density\n") +
-  xlab("\nStandard deviation") +
-  theme_bw() +
-  custom_theme
-
-# Export the figure --------------------------------------------------------
-
-path <- file.path(getwd(), "outputs", "outputs_figures")
-
-ggsave(
-  p,
-  filename = file.path(path, "figure2.png"),
-  units = "px",
-  width = 3800,
-  height = 1700,
-  dpi = 400
-)
-
-# ==========================================================================
-# ==========================================================================
-
-
-
-
-
-
-
-
-
-
-# Test ----------------------------------------------------------------------
-
-# Testing plot 2 with less information
-
-library(ggridges)
-
-p <- ggplot(data = figdat_long, aes(x = value, y = parameter, fill = model))
-
-#p + geom_violinhalf() #+ geom_jitter(width = 0.2, shape=20)
-
-fig2_test <- p + geom_density_ridges(
+fig2 <- p + geom_density_ridges(
   scale=0.75, alpha = 0.5,
   quantile_lines = TRUE,
   quantiles = 2,
   rel_min_height = 0.0009
   ) +
-  #geom_vline(
-  #  data = results,
-  #  aes(xintercept = median, color = model, linetype = parameter),
-  #  #linetype = model,
-  #  show.legend = FALSE
-  #) +
+  scale_x_continuous(breaks = seq(0, 10, 2), limits = c(0, 11)) +
   scale_fill_manual(
-    values = c("#482173FF", "#51C56AFF"),
-    labels = c("Model II: prey rank", "Model V: prey rank + prey speed")
+    values = c("#fde725", "#482173FF", "#51C56AFF"),
+    labels = c("Model I", "Model II", "Model V")
   ) +
   labs(fill = " ") +
   ylab("") +
@@ -284,13 +205,18 @@ fig2_test <- p + geom_density_ridges(
   theme_classic() +
   custom_theme
 
-fig2_test
+
+
+# Save plot ----------------------------------------------------------------
 
 path <- file.path(getwd(), "outputs", "outputs_figures")
 
 ggsave(
-  fig2_test,
+  fig2,
   filename = file.path(path, "figure2.png"),
   width = 8.5,
-  height = 6
+  height = 7.5
 )
+
+# ==========================================================================
+# ==========================================================================
