16a2b6da524cb0e3e024f559da4fa3b12a3a582a
Maxime Fraser Franco <49170767+quantitative-ecologist@users.noreply.github.com>
2025-08-13 08:52:08 -0400

    updated figure 2

--- code/code_figures/figure_2.R
+++ code/code_figures/figure_2.R
@@ -1,7 +1,7 @@
 # ==========================================================================
 
-#                          Plot regression model
-#                              of prey speed
+#                          Plot regression models
+#                         of prey speed and space
 
 # ==========================================================================
 
@@ -23,21 +23,24 @@ library(rstan)
 library(data.table)
 library(ggplot2)
 library(viridis)
+library(ggpubr)
 
 path <- file.path(getwd(), "outputs", "outputs_models")
 
-mod <- readRDS(file.path(path, "LM-PreySpeed.rds"))
+mod_speed <- readRDS(file.path(path, "LM-PreySpeed.rds"))
+mod_space <- readRDS(file.path(path, "LM-PreySpace.rds"))
 
 
 
 # Load the data ------------------------------------------------------------
 
 data <- fread(
-  "./data/FraserFrancoetal2023-data.csv",
+  "./data/FraserFrancoetal2025-data.csv",
   select = c(
     "predator_id",
     "game_duration",
     "prey_avg_speed",
+    "prey_avg_amount_tiles_visited",
     "prey_avg_rank"
   )
 )
@@ -57,7 +60,7 @@ standardize <- function(x) {
 
 data[,
   c("Zgame_duration", "Zprey_avg_rank") := lapply(.SD, standardize),
-  .SDcols = c(2, 4)
+  .SDcols = c(2, 5)
 ]
 
 # ==========================================================================
@@ -94,16 +97,25 @@ new_data <- data.frame(
 new_data$Zprey_avg_rank <- (new_data$prey_avg_rank - mean_rank) / sd_rank
 
 # Get posterior expected predictions
-epred <- posterior_epred(mod, newdata = new_data, re_formula = NA)
+epred_speed <- posterior_epred(mod_speed, newdata = new_data, re_formula = NA)
+epred_space <- posterior_epred(mod_space, newdata = new_data, re_formula = NA)
+
 
 # Convert to long format
-draws_long <- as.data.table(t(epred))
-draws_long[, prey_avg_rank := new_data$prey_avg_rank]
-draws_long <- melt(draws_long, id.vars = "prey_avg_rank", variable.name = "draw")
+draws_long_speed <- as.data.table(t(epred_speed))
+draws_long_speed[, prey_avg_rank := new_data$prey_avg_rank]
+draws_long_speed <- melt(draws_long_speed, id.vars = "prey_avg_rank", variable.name = "draw")
+
+draws_long_space <- as.data.table(t(epred_space))
+draws_long_space[, prey_avg_rank := new_data$prey_avg_rank]
+draws_long_space <- melt(draws_long_space, id.vars = "prey_avg_rank", variable.name = "draw")
 
 # Compute the median and randomly sample 50 draws
-median_preds <- draws_long[, .(value = median(value)), by = prey_avg_rank]
-sampled_draws <- draws_long[draw %in% sample(unique(draw), 50)]
+median_preds_speed <- draws_long_speed[, .(value = median(value)), by = prey_avg_rank]
+sampled_draws_speed <- draws_long_speed[draw %in% sample(unique(draw), 50)]
+
+median_preds_space <- draws_long_space[, .(value = median(value)), by = prey_avg_rank]
+sampled_draws_space <- draws_long_space[draw %in% sample(unique(draw), 50)]
 
 
 
@@ -141,54 +153,23 @@ custom_theme <- theme(
 # 3. Compute plot
 # ==========================================================================
 
-# Plot using points
-#p <- ggplot() +
-#  geom_point(
-#    data = data,
-#    aes(x = prey_avg_rank, y = prey_avg_speed),
-#    alpha = 0.05,
-#    shape = 20
-#  ) +
-#  geom_line(
-#    data = sampled_draws,
-#    aes(x = prey_avg_rank, y = value, group = draw),
-#    alpha=0.2,
-#    color="gray"
-#  ) +
-#  geom_line(
-#    data = median_preds,
-#    aes(x = prey_avg_rank, y = value),
-#    linewidth = 0.2,
-#    color="black"
-#  ) +
-#  ylab("Prey average speed\n") +
-#  xlab("\nPrey rank") +
-#  scale_y_continuous(
-#    breaks = seq(0, 4, 1), 
-#    limits = c(0, 4)
-#  ) +
-#  scale_x_continuous(
-#    breaks = seq(0, max(data$prey_avg_rank), 5), 
-#    limits = c(0, max(data$prey_avg_rank))
-#  ) +
-#  theme_classic() +
-#  custom_theme
 
-# Plot using hexbin
-p <- ggplot() +
+# Speed -----------------------------------------------------------------
+
+p1 <- ggplot() +
   geom_hex(
     data = data,
     aes(x = prey_avg_rank, y = prey_avg_speed),
     bins = 60
   ) +
   geom_line(
-    data = sampled_draws,
+    data = sampled_draws_speed,
     aes(x = prey_avg_rank, y = value, group = draw),
     alpha = 0.2,
     color = "gray"
   ) +
   geom_line(
-    data = median_preds,
+    data = median_preds_speed,
     aes(x = prey_avg_rank, y = value),
     linewidth = 0.2,
     color = "black"
@@ -201,11 +182,58 @@ p <- ggplot() +
   ) +
   scale_x_continuous(
     breaks = seq(0, max(data$prey_avg_rank), 5), 
-    limits = c(0, max(data$prey_avg_rank))
+    limits = c(0, max(data$prey_avg_rank)+1)
   ) +
   scale_fill_viridis_c(option = "D", direction = -1) +
   theme_classic() +
-  custom_theme
+  custom_theme #+theme(plot.margin = margin(5, 20, 5, 5))
+
+
+
+# Space covered ---------------------------------------------------------
+
+p2 <- ggplot() +
+  geom_hex(
+    data = data,
+    aes(x = prey_avg_rank, y = prey_avg_amount_tiles_visited),
+    bins = 60
+  ) +
+  geom_line(
+    data = sampled_draws_space,
+    aes(x = prey_avg_rank, y = value, group = draw),
+    alpha = 0.2,
+    color = "gray"
+  ) +
+  geom_line(
+    data = median_preds_space,
+    aes(x = prey_avg_rank, y = value),
+    linewidth = 0.2,
+    color = "black"
+  ) +
+  ylab("Prey average space covered\n") +
+  xlab("\nPrey rank") +
+#   scale_y_continuous(
+#     breaks = seq(0, 4, 1), 
+#     limits = c(0, 4)
+#   ) +
+  scale_x_continuous(
+    breaks = seq(0, max(data$prey_avg_rank), 5), 
+    limits = c(0, max(data$prey_avg_rank)+1)
+  ) +
+  scale_fill_viridis_c(option = "D", direction = -1) +
+  theme_classic() +
+  custom_theme# +theme(plot.margin = margin(5, 5, 5, 20))
+
+
+# Prepare figure ------------------------------------------------------------
+
+# Arrange paneled figure
+figure <- ggarrange(
+  NULL, p1, NULL, p2,
+  ncol = 4, nrow = 1,
+  labels = c("(A)", "", "(B)", ""),
+  widths = c(0.15, 1.5, 0.15, 1.5)
+)
 
 
 
@@ -214,12 +242,12 @@ p <- ggplot() +
 path <- file.path(getwd(), "outputs", "outputs_figures")
 
 ggsave(
-  p,
+  figure,
   filename = file.path(path, "figure2.png"),
   units = "in",
   dpi = 300,
-  width = 5,
-  height = 5
+  width = 10,
+  height = 4
 )
 
 # ==========================================================================
