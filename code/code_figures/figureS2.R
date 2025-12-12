b27133ad149f58b8422f71d13c326b5a4ef4f466
quantitative-ecologist <49170767+quantitative-ecologist@users.noreply.github.com>
2025-12-11 21:10:12 -0500

    fig S2 is now regression model of prey

--- code/code_figures/figureS2.R
+++ code/code_figures/figureS2.R
@@ -1,6 +1,8 @@
 # ==========================================================================
 
-#                 Plot GAMM model without rank - Figure S2
+#                          Plot regression models
+#                         of prey speed and space
+#                                Figure S2
 
 # ==========================================================================
 
@@ -18,14 +20,16 @@ options(mc.cores = parallel::detectCores())
 
 library(parallel)
 library(brms)
+library(rstan)
 library(data.table)
 library(ggplot2)
-library(ggpubr)
 library(viridis)
+library(ggpubr)
 
 path <- file.path(getwd(), "outputs", "outputs_models")
 
-mod <- readRDS(file.path(path, "GAMM-I.rds"))
+mod_speed <- readRDS(file.path(path, "LM-PreySpeed.rds"))
+mod_space <- readRDS(file.path(path, "LM-PreySpace.rds"))
 
 
 
@@ -33,18 +37,32 @@ mod <- readRDS(file.path(path, "GAMM-I.rds"))
 
 data <- fread(
   "./data/FraserFrancoetal2025-data.csv",
-  select = c("predator_id",
-             "game_duration",
-             "pred_speed",
-             "prey_avg_speed",
-             "prey_avg_amount_tiles_visited",
-             "prey_avg_rank",
-             "cumul_xp_pred",
-             "total_xp_pred",
-             "hunting_success")
+  select = c(
+    "predator_id",
+    "game_duration",
+    "prey_avg_speed",
+    "prey_avg_amount_tiles_visited",
+    "prey_avg_rank"
+  )
 )
 
+# Predator ID as factor
 data[, predator_id := as.factor(predator_id)]
+
+# Remove any NAs
+data <- data[complete.cases(data)]
+
+
+# Standardise the variables (Z-scores) -------------------------------------
+
+standardize <- function(x) {
+  (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)
+}
+
+data[,
+  c("Zgame_duration", "Zprey_avg_rank") := lapply(.SD, standardize),
+  .SDcols = c(2, 5)
+]
 
 # ==========================================================================
 # ==========================================================================
@@ -61,69 +79,50 @@ data[, predator_id := as.factor(predator_id)]
 
 # Prepare the plots --------------------------------------------------------
 
+# To unscale prey rank
+mean_rank <- mean(data$prey_avg_rank)
+sd_rank <- sd(data$prey_avg_rank)
 
-# Group-level smooths model 2
-tab_a <- conditional_effects(
-  mod, method = "fitted",
-  effects = "Zcumul_xp:predator_id",
-  robust = TRUE, re_formula = NULL
-)
-tab_a <- data.table(tab_a[[1]])
-
-# Cumulative XP global trend model 2
-tab_b <- conditional_effects(
-  mod, method = "fitted",
-  robust = TRUE, re_formula = NULL,
-  effects = "Zcumul_xp",
-  conditions = data.frame(predator_id = NA)
-)
-tab_b <- data.table(tab_b[[1]])
-
-
-# Transform values --------------------------------------------------------
-
-# Back transform x-axis values
-mean_xp <- mean(data$cumul_xp_pred)
-standev <- sd(data$cumul_xp_pred)
-sequence <- (seq(0, 500, 100) - mean_xp)
-scaled_breaks <- sequence / standev
-
-# List the tables
-tables <- list(
-  tab_a, tab_b
-)
-names(tables) <- c(
-  "tab_a", "tab_b"
+# Prepare data for predictions
+new_data <- data.frame(
+  prey_avg_rank = seq(
+    min(data$prey_avg_rank),
+    max(data$prey_avg_rank),
+    length.out = 100
+  ),
+  Zgame_duration = 0,
+  predator_id = NA
 )
 
-# Function to apply transformation
-# Computes non standardized cumulative XP
-func <- function(x) {
-  x[, cumul_xp := (Zcumul_xp * standev) + mean(data$cumul_xp_pred)]
-}
+# Standardize rank
+new_data$Zprey_avg_rank <- (new_data$prey_avg_rank - mean_rank) / sd_rank
 
-# Apply function
-lapply(tables, func)
+# Get posterior expected predictions
+epred_speed <- posterior_epred(mod_speed, newdata = new_data, re_formula = NA)
+epred_space <- posterior_epred(mod_space, newdata = new_data, re_formula = NA)
 
 
+# Convert to long format
+draws_long_speed <- as.data.table(t(epred_speed))
+draws_long_speed[, prey_avg_rank := new_data$prey_avg_rank]
+draws_long_speed <- melt(draws_long_speed, id.vars = "prey_avg_rank", variable.name = "draw")
 
-# Cut fitted values based on player XP ------------------------------------
+draws_long_space <- as.data.table(t(epred_space))
+draws_long_space[, prey_avg_rank := new_data$prey_avg_rank]
+draws_long_space <- melt(draws_long_space, id.vars = "prey_avg_rank", variable.name = "draw")
 
-# Extract player IDs with their total XP from the original data
-xp <- unique(data[, .(predator_id, total_xp_pred)])
+# Compute the median and randomly sample 50 draws
+median_preds_speed <- draws_long_speed[, .(value = median(value)), by = prey_avg_rank]
+sampled_draws_speed <- draws_long_speed[draw %in% sample(unique(draw), 50)]
 
-# Merge the two tables adding the total XP
-tab_a <- merge(tab_a, xp, by = "predator_id")
+median_preds_space <- draws_long_space[, .(value = median(value)), by = prey_avg_rank]
+sampled_draws_space <- draws_long_space[draw %in% sample(unique(draw), 50)]
 
-# Cut all matches where fitted values are above total XP
-tab_a <- tab_a[cumul_xp <= total_xp_pred, ]
 
 
 # Setup a custom theme for the plot ----------------------------------------
 
 custom_theme <- theme(
-  # Plot title
-  plot.title = element_text(size = 12),
   # axis values size
   axis.text = element_text(face = "plain",
                            size = 14,
@@ -152,78 +151,81 @@ custom_theme <- theme(
 
 
 # ==========================================================================
-# 3. Individual variance plot
+# 3. Compute plot
 # ==========================================================================
 
 
-# Model 2 no rank ----------------------------------------------------------
+# Speed -----------------------------------------------------------------
 
-p1 <- ggplot(
-  tab_a,
-  aes(x = Zcumul_xp,
-      y = estimate__ / 4,
-      color = predator_id)
-) +
-  geom_line(linewidth = 0.5, alpha = 0.25) +
-  scale_color_viridis(discrete = TRUE, option = "D") + #B
-  ylab("Hunting success\n") +
-  ggtitle("Model I") +
-  scale_y_continuous(breaks = seq(0, 1, 0.25),
-                     limits = c(0, 1)) +
-  scale_x_continuous(breaks = scaled_breaks,
-                     labels = seq(0, 500, 100)) +
-  xlab("\nCumulative experience") +
-  custom_theme
-
-# ==========================================================================
-# ==========================================================================
-
-
-
-
-
-# ==========================================================================
-# 4. Global trend plot
-# ==========================================================================
-
-# Model 2 no rank ----------------------------------------------------------
-
-p2 <- ggplot(
-  tab_b,
-  aes(x = Zcumul_xp,
-      y = estimate__ / 4)
-) +
-  geom_ribbon(
-    aes(
-      ymin = lower__ / 4,
-      ymax = upper__ / 4
-    ),
-    fill = "gray"
+p1 <- ggplot() +
+  geom_hex(
+    data = data,
+    aes(x = prey_avg_rank, y = prey_avg_speed),
+    bins = 60
   ) +
-  geom_line(linewidth = 1) +
-  ylab("Hunting success\n") +
-  ggtitle("Model I") +
+  geom_line(
+    data = sampled_draws_speed,
+    aes(x = prey_avg_rank, y = value, group = draw),
+    alpha = 0.2,
+    color = "gray"
+  ) +
+  geom_line(
+    data = median_preds_speed,
+    aes(x = prey_avg_rank, y = value),
+    linewidth = 0.2,
+    color = "black"
+  ) +
+  ylab("Prey average speed\n") +
+  xlab("\nPrey rank") +
   scale_y_continuous(
-    breaks = seq(0, 1, 0.25),
-    limits = c(0, 1)
+    breaks = seq(0, 4, 1), 
+    limits = c(0, 4)
   ) +
   scale_x_continuous(
-    breaks = scaled_breaks,
-    labels = seq(0, 500, 100)
+    breaks = seq(0, max(data$prey_avg_rank), 5), 
+    limits = c(0, max(data$prey_avg_rank)+1)
   ) +
-  xlab("\nCumulative experience") +
-  custom_theme
-
-# ==========================================================================
-# ==========================================================================
+  scale_fill_viridis_c(option = "D", direction = -1) +
+  theme_classic() +
+  custom_theme #+theme(plot.margin = margin(5, 20, 5, 5))
 
 
 
+# Space covered ---------------------------------------------------------
+
+p2 <- ggplot() +
+  geom_hex(
+    data = data,
+    aes(x = prey_avg_rank, y = prey_avg_amount_tiles_visited),
+    bins = 60
+  ) +
+  geom_line(
+    data = sampled_draws_space,
+    aes(x = prey_avg_rank, y = value, group = draw),
+    alpha = 0.2,
+    color = "gray"
+  ) +
+  geom_line(
+    data = median_preds_space,
+    aes(x = prey_avg_rank, y = value),
+    linewidth = 0.2,
+    color = "black"
+  ) +
+  ylab("Prey average space covered\n") +
+  xlab("\nPrey rank") +
+#   scale_y_continuous(
+#     breaks = seq(0, 4, 1), 
+#     limits = c(0, 4)
+#   ) +
+  scale_x_continuous(
+    breaks = seq(0, max(data$prey_avg_rank), 5), 
+    limits = c(0, max(data$prey_avg_rank)+1)
+  ) +
+  scale_fill_viridis_c(option = "D", direction = -1) +
+  theme_classic() +
+  custom_theme# +theme(plot.margin = margin(5, 5, 5, 20))
 
 
-# ==========================================================================
-# 3. Combine plots into 1 figure
-# ==========================================================================
 
 # Prepare figure ------------------------------------------------------------
 
@@ -231,12 +233,8 @@ p2 <- ggplot(
 figure <- ggarrange(
   NULL, p1, NULL, p2,
   ncol = 4, nrow = 1,
-  labels = c(
-    "(A)", "", "(B)", ""
-  ),
-  widths = c(
-    0.15, 1.5, 0.15, 1.5
-  )
+  labels = c("(A)", "", "(B)", ""),
+  widths = c(0.15, 1.5, 0.15, 1.5)
 )
 
 
@@ -245,26 +243,13 @@ figure <- ggarrange(
 
 path <- file.path(getwd(), "outputs", "outputs_figures")
 
-# If using Windows
-# ggsave(
-#   figure,
-#   filename = file.path(path, "figureS2.png"),
-#   units = "in",
-#   dpi = 300,
-#   width = 10,
-#   height = 4
-# )
-
-# If using linux
 ggsave(
+  figure,
   filename = file.path(path, "figureS2.png"),
-  plot = figure,
   units = "in",
-  width = 10,
-  height = 4,
   dpi = 300,
-  device = grDevices::png,
-  type = "cairo-png"
+  width = 10,
+  height = 4
 )
 
 # ==========================================================================
